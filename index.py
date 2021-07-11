9f5985e69e2f673d58f3b98e530a4e1082bd6187
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-07-11 19:03:24 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import random
 from matplotlib import pyplot as plt
 # print(cv.__version__)
 #this is were I started coding. First thing I am going to do -> Read an image: 
@@ -7,14 +8,15 @@ img = cv.imread('./images/1.png',0) #0 means B/W
 img2 = cv.imread('./images/2.png',0)
 
 sum = img + img2
-cv.imshow("sum", sum)
-
+# cv.imshow("sum", img2 - img) #you can do whatever calculation you want here
+noise = random.randint(0, img.shape[0]) * 10
+cv.imshow("noise", img + noise)
 print("IMAGE MATRIX", img)
 # Channel 0 is red
 # Channel 1 is green
 # Channel 2 is blue
-cv.imshow("img.png", img[:,:]) #open image in another window -> if you want only 1 channel add a third index 
-cropped = cv.imshow("img.png", img[0:450, 0:800]) #crops the img 
+#cv.imshow("img.png", img[:,:]) #open image in another window -> if you want only 1 channel add a third index 
+#cropped = cv.imshow("img.png", img[0:450, 0:800]) #crops the img 
 cv.waitKey(0)
 
 print(img.shape) #returns 3 values -> H, W and DEPTH (R G B) 
@@ -24,7 +26,7 @@ print(img[50,100]) #return the value of the specific location
 
 #CREATES HISTOGRAM FOR BW
 plt.hist(img.ravel(),256,[0,256]); 
-plt.show() #opens a window with histogram in it
+#plt.show() #opens a window with histogram in it
 
 
 #CREATES HISTORGRAM FOR COLOR 
@@ -34,5 +36,5 @@ for i,col in enumerate(color):
     #calcHist(image, channel, mask, histogram size, range)
     plt.plot(histr,color = col)
     plt.xlim([0,256])
-plt.show()
+#plt.show()
 
